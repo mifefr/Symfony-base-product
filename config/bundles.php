a1f5bd7cf0bc6966e03ae30f9db11eba17d45f07
Allan BRUYERE <mifefr@gmail.com>
2026-07-24 16:57:04 +0200

    Remove: unused API Platform bundle and api-pack dependency

--- config/bundles.php
+++ config/bundles.php
@@ -3,6 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
